424f8c48fec4af0bd1441f0db64bf54035ce8684
ufotalent <ufotalent.zju@gmail.com>
2015-01-15 17:06:59 +0800

    strand: fix bug, use a rw lock

--- include/util/strand.hpp
+++ include/util/strand.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include <cassert>
+#include <unistd.h>
 #include <atomic>
 #include <functional>
 #include <stack>
 #include "util/lock.hpp"
+#include "util/log.hpp"
 #include "service/io_service.hpp"
 
 namespace axon {
@@ -69,14 +71,24 @@ class Strand {
     }
 
     void do_dispatch() {
+        bool should_perform = false;
         bool expected = false;
+        pthread_rwlock_rdlock(&rwlock_);
         if (has_pending_tests_.compare_exchange_strong(expected, true)) {
+            should_perform = true;
+        }
+        pthread_rwlock_unlock(&rwlock_);
+        if (should_perform) {
             perform();
         }
     }
 public:
     Strand(axon::service::IOService* io_service): io_service_(io_service) {
         has_pending_tests_ = false;
+        pthread_rwlock_init(&rwlock_, NULL);
+    }
+    virtual ~Strand() {
+        pthread_rwlock_destroy(&rwlock_);
     }
 
     void dispatch(CallBack callback) {
@@ -87,17 +99,23 @@ public:
     void perform() {
         while (true) {
             assert(has_pending_tests_);
+
+            pthread_rwlock_wrlock(&rwlock_);
             bool queue_empty = queue_.empty();
             // here has_pending_tests_ = true, hence if has_pending_tests_ == queue_empty, mark it false and return
             if (has_pending_tests_.compare_exchange_strong(queue_empty, false)) {
+                pthread_rwlock_unlock(&rwlock_);
                 return;
             }
+            pthread_rwlock_unlock(&rwlock_);
+
             LockFreeQueue<CallBack>::Node* list = queue_.take_all();
             LockFreeQueue<CallBack>::Node* reversed_list = reverse_list(list);
             while (reversed_list) {
                 reversed_list->data();
                 auto last = reversed_list;
                 reversed_list = reversed_list->next;
+                // deleting last here may destroy the last shared_ptr of consistent socket, which contains this strand, this is a bug
                 delete last;
             }
         }
@@ -105,9 +123,13 @@ public:
 
     void post(CallBack callback) {
         queue_.push(std::move(callback));
-        if (!has_pending_tests_) {
+        pthread_rwlock_rdlock(&rwlock_);
+        bool should_post = !has_pending_tests_;
+        pthread_rwlock_unlock(&rwlock_);
+        if (should_post) {
             io_service_->post(std::bind(&Strand::do_dispatch, this));
         }
+        
     }
 
     template <class ...Args>
@@ -120,8 +142,7 @@ private:
     std::atomic_bool has_pending_tests_;
     LockFreeQueue<axon::service::IOService::CallBack> queue_;
     axon::service::IOService* io_service_;
-
-
+    pthread_rwlock_t rwlock_;
 };
 
 }
